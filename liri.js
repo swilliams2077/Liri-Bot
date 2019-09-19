87739345ff1811f5da20cf4fb1a1ac92bfca60bd
Shakwan Williams <shakwanwilliamsart@gmail.com>
2019-09-18 21:24:39 -0400

    working on getting spotify to work has code to mess with.

--- liri.js
+++ liri.js
@@ -1,18 +1,65 @@
 require("dotenv").config();
-
+//reuqire
 var keys = require("./keys.js");
+var spotify = require('node-spotify-api');
+var inquirer = require("inquirer");
+var axios = require('axios');
+var moment = require("moment");
+var fs = require("fs");
+//require
 
-var spotify = new spotify(keys.spotiy);
+//keys
+var spotify = new spotify(keys.spotify);
+var ombdKeys = keys.ombd.id;
+// var bandsInTownKey = keys.bandsInTown.id;
+//keys
 
+//API functions to grab info. 
 var searchConcerts = function() {
+
 
 };
 
 var searchSpotifySong = function() {
+    var songName = functionData
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        for (var i = 0; i < data.tracks.items.length; i++) {
+
+            let header = "********** SONG **********";
+            let track = "NAME:    " + data.tracks.items[i].name;
+            let artist = "ARTIST:  " + data.tracks.items[i].artists[0].name;
+            let album = "ALBUM:   " + data.tracks.items[i].album.name;
+            let release = "RELEASE: " + data.tracks.items[i].album.release_date;
+            let preview = "PREVIEW: " + data.tracks.items[i].external_urls.spotify;
+
+            console.log(header);
+            console.log(track);
+            console.log(artist);
+            console.log(album);
+            console.log(release);
+            console.log(preview);
+            console.log("");
+
+            append_log(header);
+            append_log(track);
+            append_log(artist);
+            append_log(album);
+            append_log(release);
+            append_log(preview);
+        }
+
+        console.log("");
+        setTimeout(start_liri, 4000);
+
+    });
 
 };
 
-var searchMovie = function() {
+var searchMovie = function(movieInput) {
 
 };
 
@@ -26,7 +73,7 @@ var commands = function(caseData, functionData) {
             searchConcerts();
             break;
         case 'spotify-this-song': 
-            searchSpotifySong();
+            searchSpotifySong(functionData);
             break;
         case 'movie-this': 
             searchMovie();
@@ -40,9 +87,15 @@ var commands = function(caseData, functionData) {
     }
 }
 
-var runThis = function(argOne, argTwo) {
-    command(argOne, argTwo);
+function append_log(appendText) {
+
+    fs.appendFileSync("log.txt", appendText + "\n");
 
 };
 
-runthis(process.argv[2], process.argv[3]);
+var runThis = function(argOne, argTwo) {
+    commands(argOne, argTwo);
+
+};
+
+runThis(process.argv[2], process.argv[3]);
